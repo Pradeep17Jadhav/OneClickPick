d1f739227995dab95f35ce66f40a7abb33913046
Pradeep17Jadhav <pradeep17jadhav@gmail.com>
2021-04-29 21:31:22 +0530

    refactor certificate images

--- js/CertificateSection.js
+++ js/CertificateSection.js
@@ -7,7 +7,7 @@ class CertificateSection extends Component
         this.listCertificateBox = [];
         for(let i = 0; i < 9; i++)
         {
-            let certificateBox = new CertificateBox();
+            let certificateBox = new CertificateBox(i);
             this.AddChild(certificateBox);
             this.listCertificateBox.push(certificateBox);
         }
@@ -17,17 +17,23 @@ class CertificateSection extends Component
     {
         super.Resize(width, height);
         let y = 0;
+        let thumbnailWidth, thumbnailHeight;
         for(let i = 0; i < this.listCertificateBox.length; i++)
         {
             if(width < 720)
             {
-                this.listCertificateBox[i].Resize(width, 240);
+                thumbnailWidth = width;
+                thumbnailHeight = thumbnailWidth / 1.4243;
+                this.listCertificateBox[i].Resize(thumbnailWidth, thumbnailHeight);
                 this.listCertificateBox[i].Place((width - this.listCertificateBox[i].GetWidth()) / 2, y);
                 y += this.listCertificateBox[i].GetHeight() + 10;
             }
             else if(width < 1080)
             {
-                this.listCertificateBox[i].Resize(width / 2, 240);
+                thumbnailWidth = width / 2;
+                thumbnailHeight = thumbnailWidth / 1.4243;
+
+                this.listCertificateBox[i].Resize(thumbnailWidth, thumbnailHeight);
                 if(i % 2 == 0)
                     this.listCertificateBox[i].Place(0, y);
                 else
@@ -38,7 +44,10 @@ class CertificateSection extends Component
             }
             else
             {
-                this.listCertificateBox[i].Resize(width / 3, 240);
+                thumbnailWidth = width / 3;
+                thumbnailHeight = thumbnailWidth / 1.4243;
+
+                this.listCertificateBox[i].Resize(thumbnailWidth, thumbnailHeight);
                 if(i % 3 == 0)
                     this.listCertificateBox[i].Place(0, y);
                 else if(i % 3 == 1)
@@ -58,7 +67,7 @@ class CertificateSection extends Component
 
 class CertificateBox extends Component
 {
-    constructor()
+    constructor(certIndex)
     {
         super();
         this.AddClass("certificateBox");
@@ -66,7 +75,7 @@ class CertificateBox extends Component
         this.certificate = new Certificate();
         this.certificate.SetCourseName("Introduction to TensorFlow");
         this.certificate.SetPlatform("Coursera");
-        this.certificate.SetBackgroundImage("resources/certificates/cert7.jpg");
+        this.certificate.SetBackgroundImage("resources/certificates/thumbnail_" + (certIndex + 1) + ".jpg");
         this.AddChild(this.certificate);
     }
 
